e43b04f6e85497f6f42e43a259b5a9d598c187b1
synoce-iitgn <76461801+aman1chaudhary@users.noreply.github.com>
2023-12-22 21:34:43 +0530

    hallabol23 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => {
           
           <a href="https://www.instagram.com/hallabol_iitgn/" class="youtube" target={'_blank'} without rel="noreferrer"><i class="fa-brands fa-youtube"></i></a>
           <a href="https://www.instagram.com/hallabol_iitgn/" class="linkedin" target={'_blank'} without rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a>
-          <a href="mailto:hallabol@iitgn.ac.in" class="mail" target={'_blank'} without rel="noreferrer"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
-          {/* <a href="https://www.linkedin.com/company/media-and-public-relations-committee-iit-gandhinagar/" class="linkedin"><i class="fa-brands "></i></a> */}
-          {/* <a href="https://www.linkedin.com/company/media-and-public-relations-committee-iit-gandhinagar/" class="mail"><i class="fa-brands fa-light fa-envelope"></i></a> */}
+          {/* <a href="mailto:hallabol@iitgn.ac.in" class="mail" target={'_blank'} without rel="noreferrer"><i class="fa-brands fa-envelope" aria-hidden="true"></i></a>
+          <a href="https://www.linkedin.com/company/media-and-public-relations-committee-iit-gandhinagar/" class="linkedin"><i class="fa-brands "></i></a>
+          <a href="https://www.linkedin.com/company/media-and-public-relations-committee-iit-gandhinagar/" class="mail"><i class="fa-brands fa-light fa-envelope"></i></a> */}
         </span>
       </div>
 
